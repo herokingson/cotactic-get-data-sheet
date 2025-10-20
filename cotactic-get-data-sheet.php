5b031d01ff6ca530fd018110322317cb55e6e0e9
krittinat tanasriroojipat <yourgitemail@example.com>
2025-10-20 21:16:55 +0700

    fix all

--- cotactic-get-data-sheet.php
+++ cotactic-get-data-sheet.php
@@ -259,8 +259,8 @@ function cgsd_sheet_shortcode() {
             <div class="hidden sm:block w-px bg-gray-200"></div>
 
             <div class="flex-1 p-4 px-2 md:p-6">
-                <h3 class="text-[24px] font-bold text-[#0B284D]">' . esc_html($agency) . '</h3>
-                ' . ( $desc ? '<p class="md:mt-2 text-[16px] font-sarabun leading-6 text-gray-900 h-[50px] max-h-[50px] overflow-hidden mb-0">' . esc_html($desc) . '</p>' : '' ) . '
+                <h3 class="text-[12px] font-bold font-sarabun mb-[6px] text-[#0B284D]">' . esc_html($agency) . '</h3>
+                ' . ( $desc ? '<p class="md:mt-2 text-[10px] font-sarabun leading-6 text-gray-900 h-[50px] max-h-[50px] overflow-hidden mb-[12px]">' . esc_html($desc) . '</p>' : '' ) . '
 
                 <div class="mt-1 md:mt-4 flex md:flex-wrap items-center gap-x-2 md:gap-x-2 text-sm">
                     ' . ( $website ? '
