f09b1ea05de9ec7770137f852034bb8cda6a4c05
krittinat tanasriroojipat <yourgitemail@example.com>
2026-01-07 15:55:21 +0700

    fix popup 40 list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   corePlugins: { preflight: false },
-  important: [".cgsd-tailwind","cgsd-loadding"],
+  important: ".cgsd-tailwind",
   content: [
     "./template/**/*.php",
     "./**/*.php",
